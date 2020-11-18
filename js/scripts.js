808a77dce0b04b8090771c0dbc530e19ee273a22
Serendipitydeus <calixto_jonathan@ymail.com>
2020-11-18 00:25:12 -0500

    fixed porfolio site not showing up

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-(function ($) {
+/*!
+    * Start Bootstrap - Resume v6.0.2 (https://startbootstrap.com/theme/resume)
+    * Copyright 2013-2020 Start Bootstrap
+    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+    */
+    (function ($) {
     "use strict"; // Start of use strict
 
     // Smooth scrolling using jQuery easing
@@ -9,8 +14,9 @@
             location.hostname == this.hostname
         ) {
             var target = $(this.hash);
-            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-            
+            target = target.length
+                ? target
+                : $("[name=" + this.hash.slice(1) + "]");
             if (target.length) {
                 $("html, body").animate(
                     {
